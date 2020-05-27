8648e6ec4b9f60c44e5fb2d111e32fad6150e582
harman099 <61110096+harman099@users.noreply.github.com>
2020-05-27 21:26:02 +0530

    Added addItem and removeItem function to checkout component

--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -32,7 +32,7 @@ const CheckoutPage = ({ cartItems, total }) => {
         </div>
       </div>
       {cartItems.map((cartItem) => (
-        <CheckoutItem key={cartItem.id} cartitem={cartItem} />
+        <CheckoutItem key={cartItem.id} cartItem={cartItem} />
       ))}
       <div className="total">
         <span>TOTAL: ${total}</span>
